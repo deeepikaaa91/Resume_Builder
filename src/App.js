8c7bc813532ed846d4e5513454059cc002ec992e
kunalyadav1110 <yadavkunal1110@gmail.com>
2024-06-12 16:45:57 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import {HashRouter,Routes,Route } from 'react-router-dom'
 import Technicalskills from './Pages/Technicalskills'
+import SoftSkills from './Pages/SoftSkills'
+import PersonalD from './Pages/PersonalD'
+
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Technicalskills/>}>Technicalskills</Route>
+        <Route path="/Technicalskills" element={<Technicalskills/>}>Technicalskills</Route>
+        <Route path="/" element={<PersonalD/>}>Technicalskills</Route>
+        <Route path="/SoftSkills" element={<SoftSkills/>}>Technicalskills</Route>
+
       </Routes>
     </HashRouter>
   )
