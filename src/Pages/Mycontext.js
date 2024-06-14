bf0f8854d21ab93ee602737c9e40ed529dea66bb
deeepikaaa91 <deeepikaaa91@gmail.com>
2024-06-14 14:35:41 +0530

    chec32

--- src/Pages/Mycontext.js
+++ src/Pages/Mycontext.js
@@ -3,6 +3,7 @@ import React, { createContext, useState } from 'react'
 const ContextStore = createContext()
 function Mycontext({children}) {
     const[click,setClick] = useState(0)
+    
   return (
     <ContextStore.Provider value={{click,setClick}}>{children}</ContextStore.Provider>
   )
